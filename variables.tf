44f5c5024f37eda27d49d258142ae9e8894c9cd6
cneralich <corrigan@hashicorp.com>
2019-03-04 11:53:56 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "name" {}
+
+variable "token" {}
+
+variable "oauth_token_id" {}
+
+variable "repo" {}
+
+variable "org_id" {}
+
+variable "billing_account" {}
+
+variable "iam_user_email" {}
+
+variable "organization" {}
+
+variable "projects" {
+  type        = "list"
+  default     = []
+}
+
+variable "directory" {
+    type = "list"
+    default = []
+}
